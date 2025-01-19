f81b49b6d37fced0e3ecbc1b4b5860a7ef6841cf
Hugoyu123 <263850603@qq.com>
2025-01-19 20:29:39 +0800

    Update config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -83,7 +83,7 @@ pub const RENDEZVOUS_SERVERS: &[&str] = &[
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
-    _ => "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=",
+    _ => "8eFM0vL59nrR6LxxHJnPKKZeKmu3nqit493+9GQwq0k=",
 };
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
